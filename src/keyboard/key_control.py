fe8c99e0364bf825ed9e5fc66f1cf691a6cd3ebe
vitkalovav@gmail.com <arrows364574>
2018-11-26 22:21:56 +0000

    initial

---
+++ src/keyboard/key_control.py
@@ -0,0 +1,46 @@
+import curses
+
+ 
+
+def main(win):
+
+    win.nodelay(True)
+
+    key=""
+
+    win.clear()               
+
+    win.addstr("Detected key:")
+
+    while 1:         
+
+        try:                
+
+           key = win.getkey()        
+
+           #win.clear()
+
+           win.addstr("Detected key:")
+
+           win.addstr(str(key))
+
+           if str(key) == 'KEY_RIGHT':
+               print(" right ")
+
+           if str(key) == 'q':
+               print("quit")
+               break
+
+           if key == os.linesep:
+
+              break          
+
+        except Exception as e:
+
+           # No input  
+
+           pass        
+
+ 
+
+curses.wrapper(main)
