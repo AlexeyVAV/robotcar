9fe669837a6616783e3a4e78933fd704e2cf786d
vitkalovav@gmail.com <arrows364574>
2019-01-05 21:18:03 +0000

    read sensors

--- src/sensors/arduino_ultrasonic_test_v2/arduino_ultrasonic_test_v2.ino
+++ src/sensors/arduino_ultrasonic_test_v2/arduino_ultrasonic_test_v2.ino
@@ -1,12 +1,15 @@
-// Ultrasonic Sensor
-//https://www.the-diy-life.com/connecting-an-ultrasonic-sensor-to-an-arduino/
-// versio 0.0.0
+// Ultrasonic Sensor & IR
+// versio 0.0.2
 
 int triggerPin = 11;      //Define IO pins
 int echoPin = 13;
 
 int triggerPin2 = 10;      //Define IO pins
 int echoPin2 = 12;
+
+// IR sensors
+int ir1 = 2;
+
 
 long duration;
 double distance;
@@ -22,7 +25,7 @@ void setup()
   pinMode(echoPin, INPUT);
   pinMode(triggerPin2, OUTPUT);   //Define pin
   pinMode(echoPin2, INPUT);  
-  Serial.begin(9600);           //Starts the serial communication
+  Serial.begin(19200);           //Starts the serial communication
 }
 
 void loop()
@@ -34,18 +37,24 @@ void loop()
   digitalWrite(triggerPin, LOW);
   duration = pulseIn(echoPin, HIGH); //Read the pulse travel time in microseconds.
   distance= duration*0.0344/2;        //Calculate the distance - speed of sound is 0.034 cm per microsecond
-  Serial.print("Distance 1: ");        //Display the distance on the serial monitor
-  Serial.println(distance);
+  //Serial.print("Distance 1: ");        //Display the distance on the serial monitor
+  //Serial.println(distance);
   pulseIn(echoPin, LOW);
 
   digitalWrite(triggerPin2, LOW);   //Reset the trigger pin
-  delay(200);
+  //delay(200);
   digitalWrite(triggerPin2, HIGH);     //Create a 10 micro second pulse
   delayMicroseconds(10);
   digitalWrite(triggerPin2, LOW);
   duration2 = pulseIn(echoPin2, HIGH); //Read the pulse travel time in microseconds.
   distance2= duration2*0.0344/2;        //Calculate the distance - speed of sound is 0.034 cm per microsecond
+
+  Serial.print("Distance 1: ");        //Display the distance on the serial monitor
+  Serial.print(distance);
+  Serial.print(" ; ");
   Serial.print("Distance 2: ");        //Display the distance on the serial monitor
-  Serial.println(distance2);
-    
+  Serial.print(distance2);
+  Serial.print(" ; ");
+  Serial.print("IR1 : ");
+  Serial.println(digitalRead(ir1));  
 }
