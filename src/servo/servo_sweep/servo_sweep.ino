1cf98415915d2678ce1322a5f0c38eac9b3f886b
AlexVAV <vitkalovav@gmail.com>
2019-02-24 16:59:32 +0000

    servo

--- src/servo/servo_sweep/servo_sweep.ino
+++ src/servo/servo_sweep/servo_sweep.ino
@@ -1,16 +1,7 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
 
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
 int pos = 171;    // variable to store the servo position
 int pos_var = 1;
