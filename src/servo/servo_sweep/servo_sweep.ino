70eeb82e95124df079657d338fdae1ee4e124c35
AlexVAV <vitkalovav@gmail.com>
2019-03-01 19:10:21 +0000

    added button for canon

--- src/servo/servo_sweep/servo_sweep.ino
+++ src/servo/servo_sweep/servo_sweep.ino
@@ -6,7 +6,10 @@ Servo myservo;  // create servo object to control a servo
 int pos = 171;    // variable to store the servo position
 int pos_var = 1;
 
-int servo_pin = 9; // Arduino pin to control servo
+const int servo_pin = 9; // Arduino pin to control servo
+const int buttonPin = 2; // Button pin
+
+int buttonState = 0; 
 
 int a = 0;
 
@@ -14,6 +17,9 @@ void setup() {
   Serial.begin(9600);
   myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
   myservo.write(pos);
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
 void shoot() {
@@ -35,6 +41,12 @@ void loop() {
         Serial.println("Shoot");
         shoot();
       }
+   }
+
+   buttonState = digitalRead(buttonPin);
+
+   if (buttonState == HIGH) {
+       shoot();
    }
 
 }
